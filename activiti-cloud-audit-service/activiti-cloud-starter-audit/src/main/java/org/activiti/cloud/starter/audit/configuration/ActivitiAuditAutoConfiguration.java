cd9a1b0f77805ecd32e99dd7fc44be4037824c91
dcerbo <davide.cerbo@alfresco.com>
2020-03-05 17:20:08 +0000

    AAE-2026 move properties prefix configuration in activiti-cloud module

--- activiti-cloud-audit-service/activiti-cloud-starter-audit/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditAutoConfiguration.java
+++ activiti-cloud-audit-service/activiti-cloud-starter-audit/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditAutoConfiguration.java
@@ -1,10 +1,21 @@
 package org.activiti.cloud.starter.audit.configuration;
 
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @Import(SwaggerConfig.class)
 public class ActivitiAuditAutoConfiguration {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.audit.liquibase")
+    public LiquibaseProperties auditLiquibaseProperties() {
+        return new LiquibaseProperties();
+    }
 
 }
--- activiti-cloud-query-service/activiti-cloud-starter-query/src/main/java/org/activiti/cloud/starter/query/configuration/ActivitiQueryAutoConfiguration.java
+++ activiti-cloud-query-service/activiti-cloud-starter-query/src/main/java/org/activiti/cloud/starter/query/configuration/ActivitiQueryAutoConfiguration.java
@@ -1,10 +1,21 @@
 package org.activiti.cloud.starter.query.configuration;
 
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @Import(QuerySwaggerConfig.class)
 public class ActivitiQueryAutoConfiguration {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.query.liquibase")
+    public LiquibaseProperties queryLiquibaseProperties() {
+        return new LiquibaseProperties();
+    }
 
 }
